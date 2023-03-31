44a9557aab2fac5326b8dbcb55876294876b3924
Hina Khadim <hina.khadim@edly.io>
2023-03-31 15:37:08 +0500

    new mfe of courses

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 import {
-  APP_INIT_ERROR, APP_READY, subscribe, initialize,
+  APP_INIT_ERROR, APP_READY, subscribe, initialize, getConfig,
 } from '@edx/frontend-platform';
 import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
 import ReactDOM from 'react-dom';
@@ -10,18 +10,26 @@ import ReactDOM from 'react-dom';
 import Header, { messages as headerMessages } from '@edx/frontend-component-header';
 import Footer, { messages as footerMessages } from '@edx/frontend-component-footer';
 
+import { IntlProvider, configure } from '@edx/frontend-platform/i18n';
 import appMessages from './i18n';
-import ExamplePage from './example/ExamplePage';
+import CourseCatalog from './course_catalog/CourseCatalog';
 
 import './index.scss';
 
+configure({
+  messages: appMessages,
+  config: getConfig(),
+});
+
 subscribe(APP_READY, () => {
   ReactDOM.render(
-    <AppProvider>
-      <Header />
-      <ExamplePage />
-      <Footer />
-    </AppProvider>,
+    <IntlProvider messages={appMessages.uk} locale="uk" defaultLocale="pl">
+      <AppProvider>
+        <Header />
+        <CourseCatalog />
+        <Footer />
+      </AppProvider>
+    </IntlProvider>,
     document.getElementById('root'),
   );
 });
